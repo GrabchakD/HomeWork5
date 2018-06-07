f8077219c9047db7ea1d762e013b534a608530b6
GrabchakD <dapochemu@gmail.com>
2018-06-07 23:59:29 +0300

    try to fix

--- src/main/java/LRUCache/LruCache.java
+++ src/main/java/LRUCache/LruCache.java
@@ -2,23 +2,57 @@ package LRUCache;
 
 public class LruCache {
     private int capacity;
-    private Entry head = null;
-    private Entry tail = null;
+    private int size;
+    private Entry head;
+    private Entry tail;
 
     public LruCache(int capacity) {
+        Entry temp = new Entry();
         this.capacity = capacity;
+        head = temp;
+        tail = temp;
     }
 
 
     public void put(int key, int value) {
-        /*
-        check if we have this key in our arr
-        if true set this element to head
-        else add new element to head with new key and value
-         */
+        if (size == 0) {
+            head = new Entry(key, value);
+            tail = head;
+            size++;
+            return;
+        }
 
+        Entry temp = searchEntryByKey(key);
+
+        if (temp != null) {
+            temp.value = value;
+            setElementToHead(temp);
+            return;
+        }
+
+        Entry newHead = new Entry(key, value);
+        newHead.nextElement = head;
+        head.previousElemnt = newHead;
+        head = newHead;
+        size++;
     }
 
+    public void setElementToHead(Entry entry) {
+        entry.previousElemnt.nextElement = entry.nextElement;
+
+        if (entry.nextElement == null) {
+            tail = entry.previousElemnt;
+        }
+
+        if (entry.nextElement != null) {
+            entry.nextElement.previousElemnt = entry.previousElemnt;
+        }
+
+        entry.previousElemnt = null;
+        entry.nextElement = head;
+        head.previousElemnt = entry;
+        head = entry;
+    }
 
     public int get(int key) {
         Entry entry = searchEntryByKey(key);
@@ -28,7 +62,7 @@ public class LruCache {
         }
 
         if (entry.getKey() == key) {
-            //set this element to head(create method setElementToHead
+            setElementToHead(entry);
             return entry.getKey();
         } else {
             return -1;
@@ -36,25 +70,48 @@ public class LruCache {
     }
 
     public void deleteTail(Entry entry) {
-        //delete tail
-        entry.previousElemnt.nextElement = entry.nextElement;
+        tail = tail.nextElement;
+        tail.previousElemnt = null;
+        size--;
     }
 
     public Entry searchEntryByKey(int key) {
+        Entry temp = tail;
 
-        return null;
+        for (int i = 0; i < size; i++) {
+            if (temp.key == key) {
+                return temp;
+            }
+            temp = temp.nextElement;
+        }
+
+        return temp = null;
     }
 
-    class Entry {
+    public int getSize() {
+        return size;
+    }
+
+    public class Entry {
 
         private int key;
         private int value;
         private Entry previousElemnt;
         private Entry nextElement;
 
+        public Entry() {
+        }
+
         public Entry(int key, int value) {
             this.key = key;
             this.value = value;
+        }
+
+        public Entry(int key, int value, Entry previousElemnt, Entry nextElement) {
+            this.key = key;
+            this.value = value;
+            this.previousElemnt = previousElemnt;
+            this.nextElement = nextElement;
         }
 
         public int getKey() {
